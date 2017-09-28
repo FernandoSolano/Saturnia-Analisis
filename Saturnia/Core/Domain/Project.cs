e7d3301da10b7286d472f627866f3c9d103414a9
Fernando Solano <fer_odi10@hotmail.com>
2017-09-28 17:28:33 -0600

    Fixes on Project Class

--- Saturnia/Core/Domain/Project.cs
+++ Saturnia/Core/Domain/Project.cs
@@ -10,20 +10,20 @@ namespace Core.Domain
         private String name;
         private Boolean state;
         private String description;
-        private User leader;
-        private LinkedList<User> collaborators;
+        private String estimatedHours;
+        private Datetime startDate;
+        private Datetime endDate;
 
         public Project()
         {
-            leader = new User();
-            collaborators = new LinkedList<User>();
         }
 
         public int Id { get => id; set => id = value; }
         public string Name { get => name; set => name = value; }
         public bool State { get => state; set => state = value; }
         public string Description { get => description; set => description = value; }
-        public User Leader { get => leader; set => leader = value; }
-        public LinkedList<User> Collaborators { get => collaborators; set => collaborators = value; }
+        private string EstimatedHours { get => estimatedHours; set => estimatedHours = value; }
+        private Datetime StartDate { get => startDate; set => startDate = value; }
+        private Datetime EndDate { get => endDate; set => endDate = value; }
     }
 }
