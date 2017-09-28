87f8f006ff36bb63abbd7ec5a8e839a774aaa154
Fernando Solano <fer_odi10@hotmail.com>
2017-09-28 17:40:47 -0600

    Fixed User Class

--- Saturnia/Core/Domain/User.cs
+++ Saturnia/Core/Domain/User.cs
@@ -7,18 +7,22 @@ namespace Core.Domain
     public class User
     {
         private int id;
-        private String name, nickname, password;
+        private String firstName, lastName, nickname, password;
         private Role role;
+        private LinkedList<Project> project;
 
         public User()
         {
             role = new Role();
+            project = new LinkedList<Project>();
         }
 
         public int Id { get => id; set => id = value; }
-        public string Name { get => name; set => name = value; }
+        public string FirstName { get => firstName; set => firstName = value; }
+        public string LastName { get => lastName; set => lastName = value; }
         public string Nickname { get => nickname; set => nickname = value; }
         public string Password { get => password; set => password = value; }
         public Role Role { get => role; set => role = value; }
+        public LinkedList<Project> Project { get => project; set => project = value; }
     }
 }
