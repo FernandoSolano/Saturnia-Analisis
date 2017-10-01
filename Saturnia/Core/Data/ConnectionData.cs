fef2ffaf2451eea427c14998e42ee1233c7a3a8b
Leandro <leaqjandro@gmail.com>
2017-10-01 00:05:40 -0600

    Revert "Leandro"

--- Saturnia/Core/Data/ConnectionData.cs
+++ Saturnia/Core/Data/ConnectionData.cs
@@ -7,5 +7,14 @@ namespace Core.Data
     public class ConnectionData
     {
 
+        private String connectionString;
+
+        public ConnectionData()
+        {
+            connectionString = "Data Source=163.178.173.148;Initial Catalog=Saturnia_DB;Persist Security Info=True;User ID=lenguajes;Password=lenguajes";
+        }
+
+        public string ConnectionString { get => connectionString; }
+
     }
 }
