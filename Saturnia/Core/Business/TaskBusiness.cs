348de0b7ce2f87ce6d50d47f468c83ec1175faa1
FernandoSolano <fer_odi10@hotmail.com>
2017-10-31 19:58:17 -0600

    TaskBusiness class restored

---
+++ Saturnia/Core/Business/TaskBusiness.cs
@@ -0,0 +1,37 @@
+﻿using Core.Data;
+using Core.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+namespace Core.Business
+{
+    public class TaskBusiness
+    {
+
+        private TaskData taskData;
+
+        public TaskBusiness()
+        {
+            this.taskData = new TaskData();
+        }
+
+        public Task addTask(Task task)
+        {
+            return taskData.AddTask(task);
+        }
+
+        public void DeleteTask(Task task)
+        {
+            this.taskData.DeleteTask(task);
+        }
+
+        public Task GetHoursByDateAndCollaborator(Task task)
+        {
+            return taskData.GetHoursByDateAndCollaborator(task);
+        }
+    }
+
+}
